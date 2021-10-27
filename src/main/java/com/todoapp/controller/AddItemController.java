e0d298dfeee3c10b64b770c5975524376a51308b
Pranav Bhatia <bhatiapranav1996@gmail.com>
2021-10-27 14:40:05 -0400

    Move Elements

--- src/main/java/com/todoapp/controller/AddItemController.java
+++ src/main/java/com/todoapp/controller/AddItemController.java
@@ -1,28 +1,44 @@
 package com.todoapp.controller;
 
-import java.net.URL;
-import java.util.ResourceBundle;
-
 import com.todoapp.animation.Shaker;
+
+import javafx.animation.FadeTransition;
 import javafx.fxml.FXML;
+import javafx.scene.control.Label;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
+import javafx.util.Duration;
 
 public class AddItemController {
-
-    @FXML
-    private ResourceBundle resources;
-
-    @FXML
-    private URL location;
-
     @FXML
     private ImageView addImageView;
+
+    @FXML
+    private Label noTaskLabel;
 
     @FXML
     void initialize() {
         addImageView.addEventHandler(MouseEvent.MOUSE_CLICKED, mouseEvent -> {
             new Shaker(addImageView).shake();
+
+            FadeTransition imageViewFadeTransition = new FadeTransition(Duration.millis(2000), addImageView);
+            FadeTransition noTaskLabelFadeTransition = new FadeTransition(Duration.millis(2000), noTaskLabel);
+
+            addImageView.relocate(0, 20);
+            noTaskLabel.relocate(0, 85);
+
+            addImageView.setOpacity(0);
+            noTaskLabel.setOpacity(0);
+
+            imageViewFadeTransition.setFromValue(1f);
+            imageViewFadeTransition.setToValue(0f);
+            imageViewFadeTransition.setAutoReverse(false);
+            imageViewFadeTransition.play();
+
+            noTaskLabelFadeTransition.setFromValue(1f);
+            noTaskLabelFadeTransition.setToValue(0f);
+            noTaskLabelFadeTransition.setAutoReverse(false);
+            noTaskLabelFadeTransition.play();
         });
     }
 }

b6405208233ade4f89aeb5cd50b471d94ca4fd74
Pranav Bhatia <bhatiapranav1996@gmail.com>
2021-11-10 16:00:15 -0500

    Refresh list

--- src/main/java/com/todoapp/controller/TasksListController.java
+++ src/main/java/com/todoapp/controller/TasksListController.java
@@ -6,10 +6,10 @@ import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
 import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
 
 import java.sql.ResultSet;
 import java.sql.SQLException;
-import java.sql.Timestamp;
 
 public class TasksListController {
     @FXML
@@ -24,12 +24,57 @@ public class TasksListController {
     @FXML
     private Button saveTaskButton;
 
+    @FXML
+    private ImageView refreshImageView;
+
     private ObservableList<Task> tasksObservableList = FXCollections.observableArrayList();
 
     private DBHandler dbHandler;
 
     @FXML
     void initialize() throws SQLException {
+        tasksObservableList = FXCollections.observableArrayList();
+
+        refreshList();
+
+        tasksListView.setCellFactory(taskListView -> new TaskListCellController());
+
+        saveTaskButton.setOnAction(actionEvent -> addNewTask());
+
+        refreshImageView.setOnMouseClicked(mouseEvent -> {
+            try {
+                refreshList();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        });
+    }
+
+    public void addNewTask() {
+        if (!titleTextField.getText().equals("") && !descriptionTextField.getText().equals("")) {
+            Task newTask = new Task(
+                    AddItemController.userId,
+                    titleTextField.getText().trim(),
+                    descriptionTextField.getText().trim(),
+                    null
+            );
+
+            dbHandler.insertTask(newTask);
+
+            titleTextField.setText("");
+            descriptionTextField.setText("");
+
+//            tasksObservableList.add(newTask);
+//            tasksListView.setItems(tasksObservableList);
+            try {
+                initialize();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    public void refreshList() throws SQLException {
         dbHandler = new DBHandler();
         ResultSet resultSet = dbHandler.getTasks(AddItemController.userId);
 
@@ -47,28 +92,6 @@ public class TasksListController {
         }
 
         tasksListView.setItems(tasksObservableList);
-        tasksListView.setCellFactory(taskListView -> new TaskListCellController());
-
-        saveTaskButton.setOnAction(actionEvent -> addNewTask());
-    }
-
-    public void addNewTask() {
-        if (!titleTextField.getText().equals("") && !descriptionTextField.getText().equals("")) {
-            Task newTask = new Task(
-                    AddItemController.userId,
-                    titleTextField.getText().trim(),
-                    descriptionTextField.getText().trim(),
-                    null
-            );
-
-            dbHandler.insertTask(newTask);
-
-            titleTextField.setText("");
-            descriptionTextField.setText("");
-
-            tasksObservableList.add(newTask);
-            tasksListView.setItems(tasksObservableList);
-        }
     }
 
 }
